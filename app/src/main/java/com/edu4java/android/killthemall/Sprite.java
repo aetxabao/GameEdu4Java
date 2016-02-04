eb7ac712a36702d99983ca78f99b7912979306ea
aetxabao <aitor.etxabarren@gmail.com>
2016-02-04 15:50:39 +0100

    Fourth commit

--- app/src/main/java/com/edu4java/android/killthemall/Sprite.java
+++ app/src/main/java/com/edu4java/android/killthemall/Sprite.java
@@ -2,17 +2,23 @@ package com.edu4java.android.killthemall;
 
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
-import android.graphics.Paint;
 import android.graphics.Rect;
 
+import java.util.Random;
+
 public class Sprite {
+    // direction = 0 up, 1 left, 2 down, 3 right
+    // animation = 3 back, 1 left, 0 front, 2 right
+    int[] DIR = {3, 1, 0, 2};
     private static final int BMP_ROWS = 4;
-    private static final int BMP_COLS = 3; 
+    private static final int BMP_COLS = 3;
     private int XSPEED = 5;
+    private int ZOOM = 2;
 
     private int x = 0;
     private int y = 0;
-    private int xSpeed = 5;
+    private int xSpeed;
+    private int ySpeed;
     private Bitmap bmp;
     private GameView canvas;
 
@@ -25,33 +31,38 @@ public class Sprite {
         this.bmp = bmp;
         this.width = bmp.getWidth() / BMP_COLS;
         this.height = bmp.getHeight() / BMP_ROWS;
+        Random rnd = new Random();
+        xSpeed = rnd.nextInt(20) - 10;
+        ySpeed = rnd.nextInt(20) - 10;
     }
 
     private void update() {
-        if (x > canvas.getWidth() - width - xSpeed) {
-            xSpeed = -1 * XSPEED;
-        }
-        if (x + xSpeed < 0) {
-            xSpeed = 1 * XSPEED;
+        if (x > canvas.getWidth() - ZOOM*width - xSpeed || x + xSpeed < 0) {
+            xSpeed = -xSpeed;
         }
         x = x + xSpeed;
+        if (y > canvas.getHeight() - ZOOM*height - ySpeed || y + ySpeed < 0) {
+            ySpeed = -ySpeed;
+        }
+        y = y + ySpeed;
         currentFrame = ++currentFrame % BMP_COLS;
     }
 
     public void onDraw(Canvas canvas) {
         update();
         int srcX = currentFrame * width;
-        int srcY = 1 * height;
+        int srcY = getAnimationRow() * height;
         Rect src = new Rect(srcX, srcY, srcX + width, srcY + height);
-        Rect dst = new Rect(x, y, x+width, y+height);
+        Rect dst = new Rect(x, y, x+ZOOM*width, y+ZOOM*height);
         canvas.drawBitmap(bmp, src, dst, null);
+    }
 
-        Paint paint = new Paint();
-        paint.setARGB(255, 0, 255, 0);
-        paint.setTextSize(50);
-        paint.setTextAlign(Paint.Align.CENTER);
-        canvas.drawText(Integer.toString(x), canvas.getWidth() / 2,
-                canvas.getHeight() / 2, paint);
+    // direction = 0 up, 1 left, 2 down, 3 right
+    // animation = 3 back, 1 left, 0 front, 2 right
+    private int getAnimationRow(){
+        double dirDouble = (Math.atan2(xSpeed, ySpeed) / (Math.PI / 2) + 2);
+        int dir = (int)Math.round(dirDouble) % BMP_ROWS;
+        return DIR[dir];
     }
 
 }
