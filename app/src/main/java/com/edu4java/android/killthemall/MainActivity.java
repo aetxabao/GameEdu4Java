3195906e5bac75dc4cce135ca3ac9bf7807587cd
aetxabao <aitor.etxabarren@gmail.com>
2016-02-04 15:34:06 +0100

    First commit

--- app/src/main/java/com/edu4java/android/killthemall/MainActivity.java
+++ app/src/main/java/com/edu4java/android/killthemall/MainActivity.java
@@ -2,15 +2,29 @@ package com.edu4java.android.killthemall;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
 import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
+    GameView canvas;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(new GameView(this));
+        canvas = new GameView(this);
+        setContentView(canvas);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+    }
+
+    public void onPause() {
+        super.onPause();
+        canvas.stopGame();
+    }
+
+    public void onResume() {
+        super.onResume();
+        canvas.startGame();
     }
 
 }
